ec368cf6ef40239313f2bf4be9db8692038c4ff2
mehrdad-kj <mehrdadk.jourabi@gmail.com>
2022-08-02 13:11:41 +0430

    execise of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia,
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
